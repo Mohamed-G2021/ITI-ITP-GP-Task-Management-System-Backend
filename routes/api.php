f5db6102106c248c10a44d748c72faf3a7912743
Mohamed-G2021 <mohamed_20191821@fci.helwan.edu.eg>
2023-11-08 01:57:57 +0200

    implemented sign in with google and github

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\api\GroupController;
 use App\Http\Controllers\api\PhaseController;
 use App\Http\Controllers\api\CardController;
 use App\Http\Controllers\api\InvitationController;
+use App\Http\Controllers\api\LoginController as ApiLoginController;
 use App\Http\Controllers\api\TaskController;
 use App\Http\Controllers\api\UserAttachmentController;
 use App\Http\Controllers\api\UserBoardController;
@@ -20,6 +21,7 @@ use App\Http\Controllers\api\UserCardController;
 use App\Http\Controllers\api\UserMemberController;
 use App\Http\Controllers\api\UserWorkspaceController;
 use App\Http\Controllers\api\WorkspaceController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Auth;
 
 /*
@@ -61,3 +63,7 @@ Route::delete('/auth/logout', [AuthController::class, 'logout'])
 Route::post('send-invitation', [InvitationController::class, 'sendInvitation']);
 Route::post('accept-invitation/{id}', [InvitationController::class, 'acceptInvitation']);
 Route::post('decline-invitation/{id}', [InvitationController::class, 'declineInvitation']);
+
+
+Route::get('login/{provider}', [ApiLoginController::class, 'redirectToProvider']);
+Route::get('login/{provider}/callback', [ApiLoginController::class, 'handleProviderCallback']);
